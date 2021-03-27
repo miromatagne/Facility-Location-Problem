a0b2b8b185638741535f2d21b6e245a39c450e3f
mmatagne <miromanuel.matagne@gmail.com>
2021-03-27 22:22:14 +0100

    starting code

---
+++ flp.py
@@ -0,0 +1,44 @@
+import pyomo.environ as pyo
+
+
+def read_instance(file_name):
+    opening_cost = {}
+    demand = {}
+    capacity = {}
+    travel_cost = {}
+    try:
+        file = open("Instances/{}".format(file_name), 'r')
+        info = file.readline().split(" ")
+        I = int(info[0])
+        J = int(info[1])
+        info = file.readline().split(" ")
+        for j in range(J):
+            opening_cost[j] = int(info[j])
+        info = file.readline().split(" ")
+        for i in range(I):
+            demand[i] = int(info[i])
+        info = file.readline().split(" ")
+        for j in range(J):
+            capacity[j] = int(info[j])
+        for i in range(I):
+            info = file.readline().split(" ")
+            for j in range(J):
+                travel_cost[(i, j)] = int(info[j])
+    except:
+        print("Error reading file.")
+    return opening_cost, demand, capacity, travel_cost
+
+
+def solve_flp(instance_name, linear):
+    pass
+    # return (obj,x,y)
+
+
+def initial_solution_flp(instance_name):
+    pass
+    # return (obj,x,y)
+
+
+def local_search_flp(x, y):
+    pass
+    # return (obj,x,y)
